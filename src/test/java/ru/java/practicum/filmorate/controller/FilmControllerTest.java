9e412fbc24d4594b97c02634c8ff3fc2889143db
Vicar <luckuvicar@yandex.ru>
2023-12-09 00:28:18 +0300

    Fix some methods

--- src/test/java/ru/java/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/java/practicum/filmorate/controller/FilmControllerTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.java.practicum.filmorate.exception.ValidationException;
 import ru.java.practicum.filmorate.model.Film;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.time.LocalDate;
 
 
@@ -12,6 +16,8 @@ import java.time.LocalDate;
 class FilmControllerTest {
 
     FilmController filmController;
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = validatorFactory.getValidator();
 
     @BeforeEach
     void setUp() {
@@ -46,6 +52,64 @@ class FilmControllerTest {
                 .build();
 
         Assertions.assertDoesNotThrow(() -> filmController.validate(film));
+    }
+
+    @Test
+    public void testValidFilm() {
+        Film film = Film.builder()
+                .name("Marusya")
+                .description("Description")
+                .releaseDate(LocalDate.of(1991,11,12))
+                .duration(120)
+                .build();
+
+        Assertions.assertTrue(validator.validate(film).isEmpty());
+    }
+
+    @Test
+    public void testInvalidFilmName() {
+        Film film = Film.builder()
+                .description("Description")
+                .releaseDate(LocalDate.of(1991,11,12))
+                .duration(120)
+                .build();
+
+        Assertions.assertFalse(validator.validate(film).isEmpty());
+    }
+
+    @Test
+    public void testInvalidFilmDescription() {
+        Film film = Film.builder()
+                .name("Film")
+                .description("a".repeat(201)) // >200
+                .releaseDate(LocalDate.of(1991,11,12))
+                .duration(120)
+                .build();
+
+        Assertions.assertFalse(validator.validate(film).isEmpty());
+    }
+
+    @Test
+    public void testInvalidFilmReleaseDate() {
+        Film film = Film.builder()
+                .name("Film")
+                .description("Description")
+                .duration(120)
+                .build();
+
+        Assertions.assertFalse(validator.validate(film).isEmpty());
+    }
+
+    @Test
+    public void testInvalidFilmDuration() {
+        Film film = Film.builder()
+                .name("Film")
+                .description("Description")
+                .releaseDate(LocalDate.of(1991,11,12))
+                .duration(-5)
+                .build();
+
+        Assertions.assertFalse(validator.validate(film).isEmpty());
     }
 
 }
--- src/test/java/ru/java/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/java/practicum/filmorate/controller/UserControllerTest.java
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.java.practicum.filmorate.model.User;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.time.LocalDate;
 
 
 class UserControllerTest {
 
     UserController userController;
+    private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = factory.getValidator();
 
     @BeforeEach
     void setUp() {
@@ -43,4 +49,54 @@ class UserControllerTest {
 
         Assertions.assertEquals("oda",user.getName());
     }
+
+    @Test
+    public void testEmailValidation() {
+        User user = User.builder()
+                .email("invalidEmail")  // проверка на некорректный емаил
+                .login("Login")
+                .name("totParen")
+                .birthday(LocalDate.now())
+                .build();
+
+
+        Assertions.assertFalse(validator.validate(user).isEmpty());
+    }
+
+    @Test
+    public void testLoginNotBlank() {
+        User user = User.builder()
+                .email("vlad@mamail.ru")
+                .login("")  // пустой логин
+                .name("name")
+                .birthday(LocalDate.now())
+                .build();
+
+        Assertions.assertFalse(validator.validate(user).isEmpty());
+    }
+
+    @Test
+    public void testBirthdayInFuture() {
+        User user = User.builder()
+                .email("valera@boss.com")
+                .login("login")
+                .name("nameless")
+                .birthday(LocalDate.now().plusDays(1))  //дата из будущего
+                .build();
+
+        Assertions.assertFalse(validator.validate(user).isEmpty());
+    }
+
+    @Test
+    public void testValidUserOk() {
+        User user = User.builder()
+                .email("vitalya@smeliy.com")
+                .login("login")
+                .name("stepasha")
+                .birthday(LocalDate.now())
+                .build();
+
+        Assertions.assertTrue(validator.validate(user).isEmpty());
+    }
+
 }
